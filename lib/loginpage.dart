1017bef629646b31020d2b0fb60eb7f689af4e84
Taoheedoh <taoheedoh@gmail.com>
2024-08-15 23:46:00 +0100

    Add Textfield

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -10,6 +10,41 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Center(
+              child: Icon(
+                Icons.rocket_launch_outlined,
+                size: 100,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Center(
+              child: Text(
+                'Hello again',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+
+            SizedBox(height: 20,),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
