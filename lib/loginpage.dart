db2bae6030edd1812cdfc2ccdfee3c1078a2bce4
Taoheedoh <taoheedoh@gmail.com>
2024-08-16 00:52:15 +0100

    Add login button

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:medapp/forgotpassword.dart';
+import 'package:medapp/successfulpage.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -10,37 +12,86 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
+            const SizedBox(
               height: 50,
             ),
-            Center(
+            const Center(
               child: Icon(
                 Icons.rocket_launch_outlined,
                 size: 100,
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 8,
             ),
-            Center(
+            const Center(
               child: Text(
                 'Hello again',
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
               ),
             ),
-
-            SizedBox(height: 20,),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-              ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'Login to your accout to continue',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'duke@mail.com',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {
+                      const ForgotPassword();
+                    },
+                    child: const Text('Forgot your password'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    const SuccessfulLogin();
+                  }, 
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 50),
+                  ) ,
+                  child:  const Text('Login'),
+                )
+              ],
             ),
           ],
         ),
---
+++ lib/successfulpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SuccessfulLogin extends StatefulWidget {
+  const SuccessfulLogin({super.key});
+
+  @override
+  State<SuccessfulLogin> createState() => _SuccessfulLoginState();
+}
+
+class _SuccessfulLoginState extends State<SuccessfulLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
